bb79da1966c8b784bc5965e3cf644081d73170d7
SanjayDeveloper <sureshsanjay805@gmail.com>
2020-09-19 18:36:49 +0530

    Add profile and repo lists

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="App-footer">
+                Copyright &copy; 2020
+            </footer>
+        );
+    }
+}
+export default Footer;
