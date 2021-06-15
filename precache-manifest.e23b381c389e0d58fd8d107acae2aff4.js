152aa008dedbb8686f31840e03d3989162b24e4a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-15 06:47:57 +0000

    Deploy SanjayDevTech/sanjaydevtech.github.io to github.com/SanjayDevTech/sanjaydevtech.github.io.git:gh-pages

---
+++ precache-manifest.e23b381c389e0d58fd8d107acae2aff4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a83af540ce4581305aa76f0fc88b79a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad0bb2e4c25d42770162",
+    "url": "/static/css/main.3c1df3cf.chunk.css"
+  },
+  {
+    "revision": "a29fb2c28fb2777302f3",
+    "url": "/static/js/2.0b741d82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0b741d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0bb2e4c25d42770162",
+    "url": "/static/js/main.2245c56f.chunk.js"
+  },
+  {
+    "revision": "c464f0d8face31f22d59",
+    "url": "/static/js/runtime-main.ca7e7c54.js"
+  },
+  {
+    "revision": "e72a956efdfabb883261c652a3d597b6",
+    "url": "/static/media/brand.e72a956e.png"
+  },
+  {
+    "revision": "8838b09ab6e8b06b816a1461efc56b19",
+    "url": "/static/media/dark-icon.8838b09a.svg"
+  },
+  {
+    "revision": "db0721b19bf1b047866172744342786f",
+    "url": "/static/media/github.db0721b1.svg"
+  },
+  {
+    "revision": "c4cb17e1b6783ff8a21367ad15365a82",
+    "url": "/static/media/gmail.c4cb17e1.svg"
+  },
+  {
+    "revision": "264cf33597d433c09ef11445582de0ec",
+    "url": "/static/media/light-icon.264cf335.svg"
+  },
+  {
+    "revision": "ab46037f2bb51cacbd0e7bea5a8cf3a6",
+    "url": "/static/media/linkedin.ab46037f.svg"
+  },
+  {
+    "revision": "4f41a9846006dbe195ddcf426c32c502",
+    "url": "/static/media/twitter.4f41a984.svg"
+  }
+]);
