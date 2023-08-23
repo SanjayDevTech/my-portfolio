0f7580053c59ced3165a5fa655a4f751d6ec2d44
SanjayDevTech <sureshsanjay805@gmail.com>
2023-08-23 21:14:16 +0530

    added hamburger in mobile

---
+++ src/components/hamburger/types.ts
@@ -0,0 +1,4 @@
+export default interface HamburgerProps {
+    open: boolean;
+    setOpen: (open: boolean) => void;
+}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,28 +1,38 @@
 import * as Styles from "./styles";
-
 import brandImage from "#assets/brand.png";
 import Text from "#components/text";
 import { useNavigate } from "react-router-dom";
 import GithubAccount from "#components/github-account";
+import { useMediaQuery } from "usehooks-ts";
+import { useEffect, useState } from "react";
+import Hamburger from "#components/hamburger";
 
 function Header() {
   const navigate = useNavigate();
+  const isMobile = useMediaQuery("(max-width: 576px)");
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [isMobile]);
 
   return (
     <Styles.Wrapper>
       <Styles.BrandImage src={brandImage} alt={"Brand"} />
       <Text variant="small" color="primaryDark">Sanjay</Text>
       <Styles.Grow />
-      <Styles.ActionText onClick={() => navigate("/")} color="primaryDark">
+      {isMobile
+      ? (<Hamburger open={menuOpen} setOpen={setMenuOpen} />)
+      : (<><Styles.ActionText onClick={() => navigate("/")} color="primaryDark">
         Home
       </Styles.ActionText>
-      <Styles.ActionText onClick={() => navigate("/about")} color="primaryDark">
-        About
-      </Styles.ActionText>
-      <Styles.ActionText onClick={() => navigate("/projects")} color="primaryDark">
-        Projects
-      </Styles.ActionText>
-      <GithubAccount />
+        <Styles.ActionText onClick={() => navigate("/about")} color="primaryDark">
+          About
+        </Styles.ActionText>
+        <Styles.ActionText onClick={() => navigate("/projects")} color="primaryDark">
+          Projects
+        </Styles.ActionText>
+        <GithubAccount /></>)}
     </Styles.Wrapper>
   );
 }
